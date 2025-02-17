ed505d6593605f68b36d754d1a0a565959b5467a
alvarodltp <alvarodltp@gmail.com>
2025-02-17 00:48:37 -0600

    Deployment fix.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import { DarkModeProvider } from './hooks/DarkModeContext';
-import { useEffect } from 'react';
 
 export default function App() {
   return (
